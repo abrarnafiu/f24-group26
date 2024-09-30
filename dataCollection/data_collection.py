5437d66d1f5e9c9ccc0334d5a5c3cd485e527dec
Jeffrey Pan <jeffreypan@Jeffreys-MacBook-Pro.local>
2024-09-30 16:34:09 -0400

    data collection program v1

--- dataCollection/data_collection.py
+++ dataCollection/data_collection.py
@@ -1,16 +1,29 @@
 
 import yfinance as yf
 
-inputTicker = "AAPL"
-inputTimeFrame = "1mo"
+# isValidPeriod takes in an inputPeriod and 
+def is_valid_period(input_period):
+    period = ['1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max']
+    return (input_period in period)
 
-# Fetch data for the stock
-ticker = yf.Ticker(inputTicker)
+def is_valid_ticker(input_ticker):
+    ticker = yf.Ticker(input_ticker)
+    history = ticker.history(period='1y')
+    if history.empty:
+        return False
+    else:  
+        return True
 
-# assign data to dataHistory
-dataHistory = ticker.history(period = inputTimeFrame)  # '1y' stands for one year
 
-# Display dataHistory
-print(dataHistory)
+def print_data (input_ticker, input_period):
+    if is_valid_period(input_period):
+        if is_valid_ticker(input_ticker):
+            ticker = yf.Ticker(input_ticker)
+            history = ticker.history(period='1y')
+            print(history)
+        else:
+            print("Not valid ticker")
+    else:
+        print("Not valid period")
 
-
+print_data("AAPL", "1y")
---
+++ dataCollection/tester.py
@@ -0,0 +1,14 @@
+import yfinance as yf
+
+inputTicker = "AAPL"
+inputTimeFrame = "1mo"
+
+# Fetch data for the stock
+ticker = yf.Ticker(inputTicker)
+
+# assign data to dataHistory
+dataHistory = ticker.history(period = inputTimeFrame)  # '1y' stands for one year
+
+# Display dataHistory
+print(dataHistory)
+

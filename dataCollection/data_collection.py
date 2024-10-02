77fbc9da138851d22451a707013ca81c958ec251
Jeffrey Pan <jeffreypan@Jeffreys-MacBook-Pro.local>
2024-10-02 14:21:08 -0400

    made docstrings and flipped if statments

--- dataCollection/data_collection.py
+++ dataCollection/data_collection.py
@@ -1,12 +1,12 @@
 import yfinance as yf
 
-# is_valid_period checks if the input_period is valid and returns True if it is, or False if it is not.
 def is_valid_period(input_period):
+    '''Take input_ticker and if it is valid, return #true, else return #false.'''
     period = ['1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max']
     return (input_period in period)
 
-#is_valid_ticker checks if the input_ticker is valid and returns True if it is, or False if it is not.
 def is_valid_ticker(input_ticker):
+    '''Take input_ticker and if it is valid, return #true, else return #false.'''
     ticker = yf.Ticker(input_ticker)
     history = ticker.history(period='1y')
     if history.empty:
@@ -14,17 +14,17 @@ def is_valid_ticker(input_ticker):
     else:  
         return True
 
-# print_data checks to see if both input_period and input_period are valid inputs and if so, prints the data, otherwise returns a message.
 def print_data (input_ticker, input_period):
-    if is_valid_period(input_period):
-        if is_valid_ticker(input_ticker):
+    '''Take input_ticker and input_period and if they are valid, return the data, else return a message.'''
+    if is_valid_ticker(input_ticker):
+        if is_valid_period(input_period):
             ticker = yf.Ticker(input_ticker)
             history = ticker.history(period='1y')
             print(history)
         else:
-            print("Not valid ticker")
+            print("Not valid period")
     else:
-        print("Not valid period")
+        print("Not valid ticker")
 
 print_data("AAPL", "1y")
 
